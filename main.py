ec45facbab479ca2879d275633ce9885fa347afc
Toby Morton <tmorton@student.strode-college.ac.uk>
2021-08-09 22:28:19 +0100

    v1.0

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from tkinter import *
-main = Tk()#creates a main root window
 
-def button_clicked():
-    text = Label(main,text="you clicked the button")#creates the label variable
-    text.pack()
-button_1 = Button(main,text="click me",fg="yellow",highlightbackground="blue",command=button_clicked)
-button_1.pack()#uses pack method to add button to gui
+gui = Tk()#creates gui window
+gui.title("SimpleSRS")#gives the window a title
+gui.geometry("1280x720")
 
+#create menu selection buttons
+#logo:
+logo = PhotoImage(file="logo.png")
+logo_label = Label(image=logo)
+logo_label.grid(column=0, row=0, padx="352", pady="50")
 
-main.mainloop()#prevent gui window from closing instantly
+#"browse sets" button:
+browse_text = StringVar()
+browse_button = Button(gui, textvariable=browse_text, font="Corbel", bg="#ffffff", height=3, width=30)
+browse_text.set("Browse Sets")
+browse_button.grid(column=0, row=1, padx="320")
+
+gui.mainloop()#runs the gui window
