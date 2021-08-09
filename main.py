2216946fb5ebdb125def9e5c4298b4ee90a2de0f
Toby Morton <tmorton@student.strode-college.ac.uk>
2021-08-09 20:32:55 +0100

    v1.0

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 from tkinter import *
+main = Tk()#creates a main root window
+
+def button_clicked():
+    text = Label(main,text="you clicked the button")#creates the label variable
+    text.pack()
+button_1 = Button(main,text="click me",fg="yellow",highlightbackground="blue",command=button_clicked)
+button_1.pack()#uses pack method to add button to gui
+
+
+main.mainloop()#prevent gui window from closing instantly
