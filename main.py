ca64975f75d3e76438e5608a505e6e955d5c9320
Toby Morton <tmorton@student.strode-college.ac.uk>
2021-08-12 12:09:00 +0100

    v1.00

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host='localhost',
     user='root',
-    password='tobias02',
+    password='password',
     port='3306',
     database='simple srs'
 )
