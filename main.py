108021ddb3689a3c545d1c9deaf390521d32a9bc
Toby Morton <tmorton@student.strode-college.ac.uk>
2021-08-13 16:53:37 +0100

    v1.012 created clear window function to remove repetitions of same code

--- main.py
+++ main.py
@@ -20,6 +20,11 @@ gui = Tk()  # creates gui window
 gui.title("SimpleSRS")  # gives the window a title
 gui.geometry("1280x720")  # resizes window to 1280x720p
 
+def clear_window():  #function used whenever a new page is loaded and all widgets from previous menu need to be cleared
+    all_buttons = gui.grid_slaves()  #creates list of all buttons within the gui window
+    for buttons in all_buttons:  #clears gui window
+        buttons.destroy()
+
 def fetch_sets():  # set selection menu
     # remove opening menu labels/buttons:
     logo_label.grid_forget()
@@ -35,19 +40,15 @@ def fetch_sets():  # set selection menu
         row_n += 1
 
 def set_options(chosen_set):  # management menu for chosen set, from here, lessons, reviews and edits to the set can be completed
-    all_buttons = gui.grid_slaves()  #creates list of all buttons within the gui window
-    for buttons in all_buttons:  #clears gui window
-        buttons.destroy()
+    clear_window()
     set_options_header = Label(gui, text=chosen_set, font=("Corbel", 30))  #creates a header label, the title of the chosen set
     set_options_header.grid(column=0, row=0, padx="475")  #places the header label on the canvas
-    set_manage_button = Button(set_window, text="Manage Set", command=set_management, font=("Corbel", 17), height=1, width=15)  #creates a "manage set" button
+    set_manage_button = Button(gui, text="Manage Set", command=set_management, font=("Corbel", 17), height=1, width=15)  #creates a "manage set" button
     set_manage_button.grid(column=0, row=1, padx="520", pady="5")  #places the manage set button on the canvas
 
 def set_management():
     print("set management screen")
-    all_buttons = gui.grid_slaves()  #creates list of all buttons within the gui window
-    for buttons in all_buttons:  #clears gui window
-        buttons.destroy()
+    clear_window()
 
 # OPENING MENU:
 logo = PhotoImage(file="logo.png")  # logo
